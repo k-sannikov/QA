7f42cbfd5ae02f8fa9d73b2ba4189e91ec29aa13
Kirill Sannikov <kir.sannikow@mail.ru>
2022-11-16 00:23:22 +0300

    lw9 добавил тесты для проверки поля alias при создании и редактировании товара

--- lw9/tests/shop.test.ts
+++ lw9/tests/shop.test.ts
@@ -1,5 +1,6 @@
 import { strict as assert } from 'assert';
 import lodash from 'lodash';
+import { slug } from "slug-gen";
 import { ProductsController } from "../api/controllers/products.controller";
 
 import RefParser from "@apidevtools/json-schema-ref-parser";
@@ -9,13 +10,13 @@ import { validate } from "../api/validator";
 import productData from "./test_cases/product.json";
 
 describe('Магазин', function () {
-  this.timeout(10000);
+  this.timeout(15000);
 
   let productsController: ProductsController;
   let productListSchema: RefParser.JSONSchema;
   let standartResponseSchema: RefParser.JSONSchema;
 
-  let creationResponse: any = null;
+  let creationResponse: any[] = [];
 
   before(async () => {
     productsController = new ProductsController();
@@ -25,10 +26,12 @@ describe('Магазин', function () {
     standartResponseSchema = await refParser.dereference(CONFIG.SCHEMAS_PATH + "standart_response.json");
   });
 
-  afterEach(async () => {
-    if (creationResponse) {
-      await productsController.deleteById(creationResponse.id);
-      creationResponse = null;
+  afterEach(() => {
+    if (creationResponse.length > 0) {
+      creationResponse.forEach(async element => {
+        await productsController.deleteById(element.id);
+      });
+      creationResponse = [];
     }
   });
 
@@ -40,42 +43,42 @@ describe('Магазин', function () {
   });
 
   it('успешное создание нового товара', async () => {
-    creationResponse = await productsController.create(productData.valid_product);
-    validate(standartResponseSchema, creationResponse);
+    creationResponse.push(await productsController.create(productData.valid_product));
+    validate(standartResponseSchema, creationResponse[0]);
 
     let products = await productsController.getAll();
-    let product = products.find(item => item.id == creationResponse.id);
+    let product = products.find(item => item.id == creationResponse[0].id);
 
-    assert(creationResponse.status, "статус ответа сервера 'неудача'");
+    assert(creationResponse[0].status, "статус ответа сервера 'неудача'");
     assert(product, "созданный товар не обнаружен в списке всех товаров");
     product = lodash.omit(product, ["id", "img", "cat", "alias"]);
     assert(lodash.isEqual(productData.valid_product, product), "исходные данные товара и данные нового товара отличаются");
   });
 
   it('успешное удаление товара по id', async () => {
-    creationResponse = await productsController.create(productData.valid_product);
+    creationResponse.push(await productsController.create(productData.valid_product));
 
-    let deleteResponse = await productsController.deleteById(creationResponse.id);
+    let deleteResponse = await productsController.deleteById(creationResponse[0].id);
     validate(standartResponseSchema, deleteResponse);
 
     let products = await productsController.getAll();
 
     assert(deleteResponse.status, "статус ответа сервера 'неудача'");
-    assert(!products.find(item => item.id == creationResponse.id), "удаленный товар обнаружен в списке всех товаров");
+    assert(!products.find(item => item.id == creationResponse[0].id), "удаленный товар обнаружен в списке всех товаров");
   });
 
   it('успешное редактирование существующего товара', async () => {
-    creationResponse = await productsController.create(productData.valid_product);
+    creationResponse.push(await productsController.create(productData.valid_product));
     let products = await productsController.getAll();
 
-    let modifiableProduct = products.find(item => item.id == creationResponse.id);
+    let modifiableProduct = products.find(item => item.id == creationResponse[0].id);
     modifiableProduct = { ...modifiableProduct, ...productData.valid_product_for_upd };
 
     let editResponse = await productsController.edit(modifiableProduct);
     validate(standartResponseSchema, editResponse);
 
     products = await productsController.getAll();
-    let modifiedProduct = products.find(item => item.id == creationResponse.id);
+    let modifiedProduct = products.find(item => item.id == creationResponse[0].id);
 
     modifiableProduct = lodash.omit(modifiableProduct, ["cat", "alias"]);
     modifiedProduct = lodash.omit(modifiedProduct, ["cat", "alias"]);
@@ -86,34 +89,34 @@ describe('Магазин', function () {
   });
 
   it('неуспешное создание товара с невалидными данными', async () => {
-    creationResponse = await productsController.create(productData.invalid_product);
+    creationResponse.push(await productsController.create(productData.invalid_product));
     validate(standartResponseSchema, creationResponse);
 
     let products = await productsController.getAll();
-    let product = products.find(item => item.id == creationResponse.id);
+    let product = products.find(item => item.id == creationResponse[0].id);
 
-    assert(!creationResponse.status, "статус ответа сервера 'успех'");
-    assert(!creationResponse.id, "ответ сервера содержит id созданного товара");
+    assert(!creationResponse[0].status, "статус ответа сервера 'успех'");
+    assert(!creationResponse[0].id, "ответ сервера содержит id созданного товара");
     assert(!product, "созданный товар обнаружен в списке всех товаров");
   });
 
   it('неуспешное создание товара с пустыми данными', async () => {
-    creationResponse = await productsController.create(productData.empty_product);
+    creationResponse.push(await productsController.create(productData.empty_product));
     validate(standartResponseSchema, creationResponse);
 
     let products = await productsController.getAll();
-    let product = products.find(item => item.id == creationResponse.id);
+    let product = products.find(item => item.id == creationResponse[0].id);
 
-    assert(!creationResponse.status, "статус ответа сервера 'успех'");
-    assert(!creationResponse.id, "ответ сервера содержит id созданного товара");
+    assert(!creationResponse[0].status, "статус ответа сервера 'успех'");
+    assert(!creationResponse[0].id, "ответ сервера содержит id созданного товара");
     assert(!product, "созданный товар обнаружен в списке всех товаров");
   });
 
   it('неуспешное редактирование товара невалидными данными', async () => {
-    creationResponse = await productsController.create(productData.valid_product);
+    creationResponse.push(await productsController.create(productData.valid_product));
     let products = await productsController.getAll();
 
-    let modifiableProduct = products.find(item => item.id == creationResponse.id);
+    let modifiableProduct = products.find(item => item.id == creationResponse[0].id);
     modifiableProduct = { ...modifiableProduct, ...productData.invalid_product_for_upd };
 
     let editResponse = await productsController.edit(modifiableProduct);
@@ -149,6 +152,58 @@ describe('Магазин', function () {
     let deleteResponse = await productsController.deleteById("0");
     validate(standartResponseSchema, deleteResponse);
     assert(!deleteResponse.status, "статус ответа сервера 'успех'");
+  });
+
+  it('успешная генерация поля alias у создаваемых товаров', async () => {
+    creationResponse.push(await productsController.create(productData.valid_product));
+    creationResponse.push(await productsController.create(productData.valid_product));
+    creationResponse.push(await productsController.create(productData.valid_product));
+
+    let products = await productsController.getAll();
+
+    let product1 = products.find(item => item.id == creationResponse[0].id);
+    let product2 = products.find(item => item.id == creationResponse[1].id);
+    let product3 = products.find(item => item.id == creationResponse[2].id);
+
+    assert(product1.alias == slug(productData.valid_product.title),
+      "поле alias не соответствует формату 'заголовок-товара'");
+
+    assert(product2.alias == slug(productData.valid_product.title) + "-0",
+      "поле alias не соответствует формату 'заголовок-товара-0'");
+
+    assert(product3.alias == slug(productData.valid_product.title) + "-0-0",
+      "поле alias не соответствует формату 'заголовок-товара-0-0'");
+  });
+
+  it('успешная генерация поля alias у редактируемых товаров', async () => {
+    creationResponse.push(await productsController.create(productData.valid_product));
+    creationResponse.push(await productsController.create(productData.valid_product));
+    creationResponse.push(await productsController.create(productData.valid_product));
+
+    let products = await productsController.getAll();
+
+    let product1 = products.find(item => item.id == creationResponse[0].id);
+    await productsController.edit({ ...product1, ...productData.valid_product_for_upd });
+
+    let product2 = products.find(item => item.id == creationResponse[1].id);
+    await productsController.edit({ ...product2, ...productData.valid_product_for_upd });
+    
+    let product3 = products.find(item => item.id == creationResponse[2].id);
+    await productsController.edit({ ...product3, ...productData.valid_product_for_upd });
+
+    products = await productsController.getAll();
+    product1 = products.find(item => item.id == creationResponse[0].id);
+    product2 = products.find(item => item.id == creationResponse[1].id);
+    product3 = products.find(item => item.id == creationResponse[2].id);
+
+    assert(product1.alias == slug(productData.valid_product_for_upd.title),
+      "поле alias не соответствует формату 'заголовок-товара'");
+
+    assert(product2.alias == slug(productData.valid_product_for_upd.title) + "-" + product2.id,
+      "поле alias не соответствует формату 'заголовок-товара-id'");
+
+    assert(product3.alias == slug(productData.valid_product_for_upd.title) + "-" + product3.id,
+      "поле alias не соответствует формату 'заголовок-товара-id'");
   });
 
 });
